e3deed66b5f8e1b5b4862ab8aa3b8455a1d16055
chervotkina <elenasheshukova@gmail.com>
2023-03-20 11:25:13 +0300

    add unuttests for LoginWorker, fix unites for TaskRepositoryStub

---
+++ TodoListTests/LoginWorkerTests.swift
@@ -0,0 +1,46 @@
+//
+//  LoginWorkerTests.swift
+//  TodoListTests
+//
+//  Created by Елена Червоткина on 20.03.2023.
+//
+
+import XCTest
+@testable import TodoList
+
+final class LoginWorkerTests: XCTestCase {
+
+	func test_login_withValidData_shouldBeSuccess() {
+
+		// arrange
+		let sut = makeSUT()
+
+		// act
+		let result = sut.login(login: "1", password: "1")
+
+		// assert
+		XCTAssertTrue(
+			result,
+			"Ошибка авторизации с валидным логином и паролем"
+		)
+	}
+
+	func test_login_withNotValidData_shouldBeFailure() {
+
+		// arrange
+		let sut = makeSUT()
+
+		// act
+		let result = sut.login(login: "2", password: "2")
+
+		// assert
+		XCTAssertFalse(
+			result,
+			"Ошибка, успешная авторизация с невалидными логином и паролем"
+		)
+	}
+
+	private func makeSUT() -> LoginWorker {
+		LoginWorker()
+	}
+}
